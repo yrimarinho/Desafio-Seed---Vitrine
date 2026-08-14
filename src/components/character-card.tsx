595e78c134746f2b5df84573f9ce906d2ff63407
Yuri <marinhoy870@gmail.com>
2026-08-14 12:24:28 -0300

    style: mudança visual em character-card

--- src/components/character-card.tsx
+++ src/components/character-card.tsx
@@ -3,9 +3,9 @@ import Character from "../types/character";
 // função recebe um objeto do tipo Character que respeita a interface Character
 export default function CharacterCard({ character }: { character: Character }) {
   return (
-    <div className="bg-white rounded-lg shadow-md p-4">
+    <div className="bg-white shadow-md p-4">
       <img src={character.image} alt={character.name} className="w-full h-auto rounded-md" />
-      <h2 className="text-xl font-bold mt-2">{character.name}</h2>
+      <h2 className="text-green-500 font-bold mt-2">{character.name}</h2>
       <p className="text-gray-600">Status: {character.status}</p>
       <p className="text-gray-600">Species: {character.species}</p>
     </div>

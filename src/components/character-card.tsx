4b7ee5b9a46503850836137d765ef0446752cf46
Yuri <marinhoy870@gmail.com>
2026-08-14 10:13:22 -0300

    feat: criando interface de personagens e o componente character-card

---
+++ src/components/character-card.tsx
@@ -0,0 +1,12 @@
+import Character from "../types/character";
+
+export default function CharacterCard({ character }: { character: Character }) {
+  return (
+    <div className="bg-white rounded-lg shadow-md p-4">
+      <img src={character.image} alt={character.name} className="w-full h-auto rounded-md" />
+      <h2 className="text-xl font-bold mt-2">{character.name}</h2>
+      <p className="text-gray-600">Status: {character.status}</p>
+      <p className="text-gray-600">Species: {character.species}</p>
+    </div>
+  );
+};
